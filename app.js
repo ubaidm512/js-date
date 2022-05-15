c9ec77ebf1549e5fa2156394630857eac3deff43
ubaidm512 <98470646+ubaidm512@users.noreply.github.com>
2022-05-15 13:03:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+///////////////////////////////function//////////////////////////
+
+// qustion no 1
+
+// var jack = new Date();
+// console.log(jack)
+
+// question no 2
+
+// function greet() {
+//     var firstName="muhammad";
+//     var lastName ="ubaid";
+//     var fullName = firstName +  ""  + lastName;
+//     document.write(fullName)
+    
+// }
+
+// greet();
+
+
+// question no 3
+
+
+// function sum(){
+// console.log(20+40)
+// }
+// sum();
+
+
+// question no 4
+
+
+// function sum( num1,num2=0 ){
+//     document.write(num1+num2);
+// }
+// sum(20+40);
+
+// question no 5
+
+// function greet() {
+//   console.log("jawan pakistan");
+//   return ;
+// }
+
+// greet();
+// question no 6
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+//     return num1 + num2;
+// }
+
+// var value = sum(10, 400);
+// console.log("value", value);
+
+// question no 7
+
+// function sum() {
+//     var FirstNumber = 1;
+//     var SecondNumber = 5;
+
+//     var result = 0;
+
+//     for (var i = FirstNumber; i <= SecondNumber; i++) {
+//       result += i;  
+//     }
+
+//     alert(result);
+// }
+
+// sum()
+// question no 11
+
+
+// function cap(value) {
+//   var capWord = value[0] .toUpperCase() + value.slice(1) ;
+//   return capWord;
+// }
+
+// var value = cap("the quick brown fox");
+
+// console.log(value)
+
+// question no 12
+// function longWord(str){
+//     var newString = str.split("");
+//     newString.sort()(function(a,b){return b.lenght - a.length});
+//     return newString[0];
+// }
+// console.log(longWord("Web Development Tutorial"));
+
+
+
+///////////////date methed/////////////
+
+// question no 1
+
+// var now = new Date();
+// console.log(now);
+  
+
+// question no 2
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// var now = new Date().getDay();
+// console.log(now);
+
+// console.log(dayNames[now]);
+
+// question no 3
+
+// function hill(message = " satuday and sunday") {
+//     alert(message);
+//   }
+//   hill("It's fun day");
+
+// question no 5
+
+// var now = new Date();
+
+// document.write("current date " + new Date( )+"<br>");
+// document.write( "Elapsed minutes january 1. 1970 "+now.getMinutes()+"<br>");
+// document.write("Elapsed milliseconds since january 1. 1970"+now.getMilliseconds());
+
+
+
